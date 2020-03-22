62858faff6ee172d63e0b974f310974c8ee4c52f
alpineQ <mkgolitsino@mail.ru>
2020-03-22 11:46:49 +0300

    Небольшие исправления + перевод

--- split_this_please.py
+++ split_this_please.py
@@ -1,36 +1,21 @@
 """ Разделение данных для шифра разнозначной замены """
-
 # pylint: disable=invalid-name
-text = ''
-with open('SRZ.txt') as file:
-    for line in file:
-        text += line[0:-1]
 
-bigram_freq = {}
-for index, elem in enumerate(text):
-    bigram = text[index - 1] + text[index]
-    if bigram not in bigram_freq:
-        bigram_freq[bigram] = 1
-    else:
-        bigram_freq[bigram] += 1
+filepath = 'SRZ.txt'
+print(f'Беру данные из {filepath}')
+with open(filepath) as file:
+    text = file.read().replace('\n', '')
 
-digit_freq = {}
 
-for bigram in bigram_freq:
-    first_digit = str(bigram)[0]
-    if first_digit not in digit_freq:
-        digit_freq[first_digit] = bigram_freq[bigram]
-    else:
-        digit_freq[first_digit] += bigram_freq[bigram]
-
+digit_freq = {letter: text.count(letter) for letter in text}
 digit_freq_list = list(digit_freq.items())
-digit_freq_list.sort(key=lambda i: i[1])
+digit_freq_list.sort(key=lambda i: i[1], reverse=True)
 
-most_freq_digit = [digit_freq_list[-1][0], digit_freq_list[-2][0], digit_freq_list[-3][0]]
+most_freq_digit = [digit_freq_list[0][0], digit_freq_list[1][0], digit_freq_list[2][0]]
 
-print('Reformatting, assuming these are most frequent digits:')
+print('Расставляю пробелы, предполагая, что эти цифры наиболее частотные:')
 for digit in most_freq_digit:
-    print(f'{digit} found {digit_freq[digit]} times')
+    print(f'{digit} найдена {digit_freq[digit]} раз')
 
 formatted_text = text
 ft_index = 0
@@ -47,6 +32,6 @@ while index < len(text):
         index += 1
 
 with open("formatted_text.txt", "w") as file:
-    file.write(formatted_text)
+    file.write(formatted_text.rstrip())
 
-print('Check formatted_text.txt')
+print('\nЗаписано в formatted_text.txt')
